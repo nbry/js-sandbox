871b6b318ad520fdcf8ba4be80e6076a4e0ec27c
nbry <nwbryant@umich.edu>
2021-11-12 22:41:25 -0800

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 13
+	},
+	'plugins': [
+		'prettier',
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const lib = require('./lib');
+
+
+
+lib.cryptoHmac();
